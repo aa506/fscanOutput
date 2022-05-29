9ab043602910c041dc823738cced4ca114974a89
aa506 <29257678+aa506@users.noreply.github.com>
2022-05-29 11:53:06 +0800

    Update nuclei_out.py

--- nuclei_out.py
+++ nuclei_out.py
@@ -5,10 +5,8 @@ import os,sys
 file1 = sys.argv[1]
 outfile = sys.argv[1]
 
-filename = open(file1,'r') #后续弄成参数传递
+filename = open(file1,'r') 
 with open(outfile+'.html','w') as e:
 	for line in filename.readlines():
-		# if ' ' in line:
-		# 	line = '<td><tr>'+line+ '</td></tr>'
 		e.write("<pre>" + line + "</pre>")
 
